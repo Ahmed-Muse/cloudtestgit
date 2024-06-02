b7d56176560e5b28827d3141af938f3dd1d35452
Rayxaan <ahmeddove114@gmail.com>
2024-06-02 07:56:42 +0300

    test

--- allifmaaluiapp/urls.py
+++ allifmaaluiapp/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 app_name='allifmaaluiapp'
 urlpatterns = [
-path('UI/Home/', views.allifmaalUIHome, name="allifmaalUIHome"),#this is the home page
+    
+path('UI/Home/', views.allifmaalUIHome, name="allifmaalUIHome"),
+path('UI/Full/Single/Table/', views.allifmaalUIFullSingleTable, name="allifmaalUIFullSingleTable"),
+
+
+
+path('UI/Home/Full/', views.allifmaalUIHomeFull, name="allifmaalUIHomeFull"),#this is the home page
 path('UI/Home/Full/Table/', views.allifmaalUIFullTable, name="allifmaalUIFullTable"),
 
 path('UI/Home/Full/Half/Table/', views.allifmaalUISmallTable, name="allifmaalUISmallTable"),
--- allifmaaluiapp/views.py
+++ allifmaaluiapp/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
+#from allifmaalapp.forms import AddAllifmaalQuoteDetailsForm,AddAllifmaalTasksForm
+#from allifmaalapp.models import AllifmaalTasksModel,AllifmaalCustomersModel
 
 # Create your views here.
 def allifmaalUIHome(request):
+    #form =AddAllifmaalTasksForm(request.POST or None)
+    title="Home - Pending Tasks"
+    #tasks=AllifmaalTasksModel.objects.all()
+    context={
+        "title":title,
+        #"form":form,
+        #"tasks":tasks,
+    }
+    return render(request,'allifmaaluiapp/home/home.html',context)
+
+def allifmaalUIFullSingleTable(request):
+    title="Full Single Table"
+    #form=AddAllifmaalQuoteDetailsForm()
+    #customers=AllifmaalCustomersModel.objects.all()
+    context={
+        "title":title,
+        #"form":form,
+        #"customers":customers,
+    }
+    return render(request,'allifmaaluiapp/tables/fullsingletable.html',context)
+
+
+def allifmaalUIHomeFull(request):
     context={}
     return render(request,'allifmaaluiapp/uihome.html',context)
 
 def allifmaalUIFullTable(request):
     title="Full Table"
-    
+    #form=AddAllifmaalQuoteDetailsForm()
     context={
         "title":title,
-       
+        #"form":form,
     }
     return render(request,'allifmaaluiapp/tables/fulltable.html',context)
 def allifmaalUISmallTable(request):
